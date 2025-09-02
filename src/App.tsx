06ecd4b816b5403fffffd5c39736022d2fad4faf
Niko Raes <niko.raes@arcadis.com>
2025-09-02 19:13:23 +0200

    feat: add custom hook useTheme for theme management
    - Implemented useTheme hook to manage light and dark themes.
    - Utilizes localStorage to persist user theme preference.
    - Checks system color scheme preference on initial load.
    - Provides toggleTheme function to switch between themes.
    - Updates document root class and localStorage on theme change.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import HomePage from './HomePage'
+import HomePage from './HomePage.tsx'
 
 function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
   		}
   	},
   	extend: {
+		fontFamily: {
+			sans: ["Plus Jakarta Sans", "sans-serif"],
+		},
   		colors: {
   			border: 'hsl(var(--border))',
   			input: 'hsl(var(--input))',
@@ -63,7 +66,21 @@ module.exports = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
   	}
   },
   plugins: [require("tailwindcss-animate")],
